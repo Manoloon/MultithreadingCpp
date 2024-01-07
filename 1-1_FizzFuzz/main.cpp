c7a9dde2675885cdaac8aea66f8ad6d69a2aa225
Manoloon <pablo.r.sanchez@hotmail.com>
2024-01-07 15:49:56 +0100

    FizzBuzz

--- 1-1_FizzFuzz/main.cpp
+++ 1-1_FizzFuzz/main.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
 #include <thread>
 #include <iostream>
-void FizzBuzz(int n){
-    switch (n) {
-        case expression:
-        
+#include <string>
+#include <vector>
+
+class FizzBuzz {
+public:
+    void operator()(std::string name,int n){
+        std::string result;
+        if (n %3 == 0)
+            result = "Fizz";
+        if (n % 5 == 0)
+            result += "Buzz";
+        std::cout << name << " says " << result << "\n";
     }
-}
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    FizzBuzz fizzBuzz;
+    std::vector<std::string> childs{"Abdul","Bart","Claudia","Divya"};
+
+    for(int i = 1;i < 5;i++){
+        std::thread thr1(fizzBuzz,childs[i],i);
+        thr1.join();
+    }
+
     return 0;
 }
