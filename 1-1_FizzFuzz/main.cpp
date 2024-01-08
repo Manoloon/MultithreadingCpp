0d93156464caee9104b37b742dd85b722f633223
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2024-01-08 07:52:17 +0100

    FizzBuzz complete

--- 1-1_FizzFuzz/main.cpp
+++ 1-1_FizzFuzz/main.cpp
@@ -2,28 +2,36 @@
 #include <thread>
 #include <iostream>
 #include <string>
-#include <vector>
+#include <array>
 
 class FizzBuzz {
 public:
-    void operator()(std::string name,int n){
+    void operator()(std::string name, int n) {
         std::string result;
-        if (n %3 == 0)
+        if (n % 3 == 0)
             result = "Fizz";
         if (n % 5 == 0)
             result += "Buzz";
+        else
+            result= std::to_string(n);
+
         std::cout << name << " says " << result << "\n";
     }
 };
 
 int main() {
     FizzBuzz fizzBuzz;
-    std::vector<std::string> childs{"Abdul","Bart","Claudia","Divya"};
-
-    for(int i = 1;i < 5;i++){
-        std::thread thr1(fizzBuzz,childs[i],i);
-        thr1.join();
+    std::array<std::string, 4> childrens{"Abdul", "Bart", "Claudia", "Divya"};
+    int count = 0;
+    for (int i = 0; i < 10005; i++) {
+        std::string name = childrens[count];
+        if (count == childrens.size()-1) {
+            count=0;
+        } else {
+            count++;
+        }
+        std::thread thr(fizzBuzz, name, i);
+        thr.join();
     }
-
     return 0;
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MultithreadingCpp
-Assignments for the course 
+Assignments for the course Multithreading with Modern C++ by James Raynard
+
+1_1-FizzBuzz : Launching a Thread.
