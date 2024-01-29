f6b034fec5e341a29669822f30a482bfc0224823
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2024-01-29 08:34:52 +0100

    packagedtask assigment

--- 8-2_PackagedTaskAssigment/main.cpp
+++ 8-2_PackagedTaskAssigment/main.cpp
@@ -3,19 +3,29 @@
 #include <iostream>
 #include <thread>
 #include <future>
+
 using namespace std::chrono_literals;
+std::mutex print_mut;
+void ProduceSum(std::promise<int>& sum,int a,int b){
+    int result = a + b;
+    std::this_thread::sleep_for(2s);
+    sum.set_value(result);
+}
+void ConsumeSum(std::shared_future<int>& fSum){
+    std::unique_lock print_lck(print_mut);
+    std::cout << "Waiting for future: " << std::endl;
+    int sum = fSum.get();
+    std::cout << "Future sum is here : " << sum << std::endl;
+}
 int main() {
-    std::packaged_task<int(int,int)> ptest([](int a, int b){
-        std::this_thread::sleep_for(2s);
-        return a * b;
-    });
-    std::future<int> fut = ptest.get_future();
-    //ptest(2,3);
-    // remember that this type of class is only movable.
-    std::thread thr(std::move(ptest),2,8);
-    thr.join();
-    std::cout << "waiting the 2s :" << std::endl;
-    std::cout  << fut.get() << std::endl;
+    std::promise<int> promise;
+    std::shared_future<int> fut = promise.get_future();
+
+    std::thread thrProd(ProduceSum,std::ref(promise),2,3);
+
+    std::thread thrCons(ConsumeSum,std::ref(fut));
+    thrProd.join();
+    thrCons.join();
 
     return 0;
 }
