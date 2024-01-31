829f21c96945e4d7fc59656072be3d177297b6c3
Pablo Sanchez <pablo.r.sanchez@hotmail.com>
2024-01-31 19:36:24 +0100

    vscode version

--- 9_2-standardAlgos/main.cpp
+++ 9_2-standardAlgos/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
-#include <execution>
+#include <vector>
+#include <numeric>
+//#include <execution>
 
 int main() {
     std::string word = "Califragilistico";
@@ -11,12 +13,13 @@ int main() {
     (it != std::cend(word))?
         std::cout << " find the first appearance of : " << letter << " in position n : " << std::distance(std::cbegin(word),it) << std::endl :
         std::cout << letter << " not found" << std::endl;
+        /*
     std::cout << "Accumulate " << std::endl;
     std::vector<int> vec1={1,2,3,4};
     std::vector<int> vec2(vec1.size());
     std::inclusive_scan(std::execution::par,std::cbegin(vec1),std::cend(vec1),vec2);
     for(auto v : vec2){
         std::cout << v << ", " ;
-    }
+    }*/
     return 0;
 }
