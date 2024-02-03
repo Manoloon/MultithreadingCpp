d971da5bca228e9279d091a3b9bbdf78d456c944
Pablo Sanchez <pablo.r.sanchez@hotmail.com>
2024-02-03 09:53:00 +0100

    shared ptr threads

--- 10_2-SharedPtrThreads/main.cpp
+++ 10_2-SharedPtrThreads/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
-
+#include <memory>
+#include <thread>
+#include <mutex>
+auto shr1 = std::make_shared<int>(42);
+std::mutex mut;
+void func(){
+    std::lock_guard<std::mutex> lck(mut);
+    auto shr2 = shr1;
+    *shr1 = *shr1 * 2;
+    std::cout << "func1 id : " << std::this_thread::get_id() << " shared :" << *shr2 << std::endl;
+    std::cout << "func1 : " << " shared 1 :" << *shr1 << std::endl;
+}
+void func2(){
+    std::lock_guard<std::mutex> lck(mut);
+    auto shr3 = shr1;
+    std::cout << "func2 id : " << std::this_thread::get_id() << " shared :" << *shr3 << std::endl;
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "shr1 :" << *shr1 << std::endl;
+    std::thread thr1(func);
+    std::thread thr2(func2);
+    thr1.join();
+    thr2.join();
     return 0;
 }
